3b0194d1fb9104510d9308fb96374565de50a6ac
s.zvyagin <s.zvyagin@gaijin.team>
2021-10-18 00:45:09 +0300

    new modules example
    create explicit dependency via library in makefile

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-import hello;
-import privstat;
+#include <stdio.h>
+#include <string>
+
+import math;
 
 int main() {
-  return hello_world();
+  math::Vec3 x{1, 0, 0};
+  math::Vec3 y{0, 1, 0};
+  math::Vec3 z{0, 0, 1};
+
+  math::Vec3 xy = add(x, y);
+  math::Vec3 xz = x + z + math::v3zero;
+  printf("%f %d\n", math::dot(xy, xz), math::random());
+  printf("%s\n", to_string(xz).c_str());
+  return 0;
 }
---
+++ math.cpp
@@ -0,0 +1,41 @@
+module;
+#include <stdlib.h>
+#include <string>
+export module math;
+export namespace math
+{
+
+template<typename A, typename B>
+auto add(A a, B b) -> decltype(a+b)
+{
+  return a + b;
+}
+
+struct Vec3
+{
+  float x, y, z;
+};
+
+Vec3 operator+(Vec3 const& l, Vec3 const& r)
+{
+  return {l.x + r.x, l.y + r.y, l.z + r.z};
+}
+
+float dot(Vec3 const& l, Vec3 const& r)
+{
+  return l.x*r.x + l.y*r.y + l.z*r.z;
+}
+
+int random()
+{
+  return ::rand();
+}
+
+Vec3 v3zero{0, 0, 0};
+
+std::string to_string(Vec3 const& v)
+{
+  return "x = " + std::to_string(v.x) + ", y = " + std::to_string(v.y) + ", z = " + std::to_string(v.z);
+}
+
+}
